63c40f87dbdf25d72757b899afc130291e5c11b0
kun.ma <makun5201@163.com>
2025-07-24 14:42:15 +0800

    repackage

--- Projects/springcloud-demo/work-service/src/main/java/com/example/work/WorkServiceApplication.java
+++ Projects/springcloud-demo/work-service/src/main/java/com/example/work/WorkServiceApplication.java
@@ -1,16 +1,17 @@
 package com.example.work;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@Slf4j
+@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient  // 启用服务注册与发现
 public class WorkServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkServiceApplication.class, args);
-//        log.info("work-service has started >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        log.info("work-service has started >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
     }
 }
