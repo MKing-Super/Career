a5ac42ebab003efa285233fcf7e2dcd8d1d19753
kun.ma <makun5201@163.com>
2025-08-07 17:59:43 +0800

    md update~

---
+++ Projects/springcloud-demo/home-admin/src/main/java/com/example/home/admin/listener/FeignRequestInterceptor.java
@@ -0,0 +1,10 @@
+package com.example.home.admin.listener;/**
+ * @describe: TODO
+ * @Author MK
+ * @PackageName springcloud-demo
+ * @Package com.example.home.admin.listener
+ * @Date 2025/8/1 11:43
+ * @Version 1.0
+ */
+public class FeignRequestInterceptor {
+}
