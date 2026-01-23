b20437a9dc604a190f185bc6c7ed999b8b890508
kun.ma <makun5201@163.com>
2026-01-23 15:15:42 +0800

    update daily

--- Projects/springcloud-demo/home-admin/src/main/java/com/example/home/admin/controller/NettyController.java
+++ Projects/springcloud-demo/home-admin/src/main/java/com/example/home/admin/controller/NettyController.java
@@ -1,27 +1,38 @@
 package com.example.home.admin.controller;
 
 import com.example.home.utils.websocket.client.NettyClient;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Slf4j
 @Controller
 @RequestMapping("/netty")
 public class NettyController {
+    private static final Logger log = LoggerFactory.getLogger(NettyController.class);
 
+    // netty 发送信息测试页面
     @GetMapping("/test")
     public String test(){
-        return "/netty/test";
+        log.info("enter sendMsgTest page~");
+        return "netty/ClientTest";
     }
 
+    // 发送信息测试页面 ajax调用接口发送到WebSocket
     @GetMapping("/receiveMsg")
     @ResponseBody
     public String receiveMsg(String msg){
         NettyClient.getInstance().writeAndFlush(msg);
         return "success~";
+    }
+
+    // Websocket页面测试
+    @GetMapping("/webSocketTest")
+    public String webSocketTest(){
+        log.info("enter webSocketTest page~");
+        return "netty/WebsocketTest";
     }
 
 }
