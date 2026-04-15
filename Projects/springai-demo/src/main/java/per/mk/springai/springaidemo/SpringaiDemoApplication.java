19fc6957b859dd5f661258892c4ab36c1dbd89a3
kun.ma <makun5201@163.com>
2026-04-15 09:57:23 +0800

    update daily

---
+++ Projects/springai-demo/src/main/java/per/mk/springai/springaidemo/SpringaiDemoApplication.java
@@ -0,0 +1,13 @@
+package per.mk.springai.springaidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiDemoApplication.class, args);
+    }
+
+}
---
+++ Projects/springai-demo/src/test/java/per/mk/springai/springaidemo/SpringaiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package per.mk.springai.springaidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
