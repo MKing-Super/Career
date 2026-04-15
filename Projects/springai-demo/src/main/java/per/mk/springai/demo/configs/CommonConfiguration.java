a9c2bfa62bb8b4bee150e5882a08ad899db90cc4
kun.ma <makun5201@163.com>
2026-04-15 16:15:01 +0800

    Company update~

--- Projects/springai-demo/src/main/java/per/mk/springai/demo/configs/CommonConfiguration.java
+++ Projects/springai-demo/src/main/java/per/mk/springai/demo/configs/CommonConfiguration.java
@@ -1,7 +1,10 @@
 package per.mk.springai.demo.configs;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +13,20 @@ import org.springframework.context.annotation.Configuration;
 public class CommonConfiguration {
 
     @Bean
-    public ChatClient chatClient(OpenAiChatModel model) {
+    public ChatClient chatClient(OpenAiChatModel model,ChatMemory chatMemory) {
         return ChatClient.builder(model)
                 .defaultSystem("你的名字叫阿坤。")
                 .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
                 .build();
 
     }
+
+
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+
+
 }
